daf99ae8caf348a12267f1db180fdec15d242177
mar-esther23 <mar.esther23@gmail.com>
2015-11-30 00:16:23 -0600

    Fixed genes section with labeling and ploting

--- BoolNet-extensions.R
+++ BoolNet-extensions.R
@@ -57,4 +57,82 @@ labelAttractors <- function(attr, node.names, labels, rules) {
 
 
 
+#######################
+####   FIX NODES   ####
+#######################
+getStatesAndBasin <- function (attr) {
+    # Receives a set of attractors, 
+    # returns a list with int_state and basin
+    # if the basin value is NA returns TRUE
+    states <- sapply(attr$attractors, function(attractor) attractor$involvedStates )
+    basin <- sapply(attr$attractors, function(attractor) {
+        if (is.na(attractor$basinSize)) TRUE  #control for asynchronous
+        else attractor$basinSize  #basin size in synchronous
+    })
+    states <- list(states=states, basin=basin)
+    #names(states) = states
+    states
+}
 
+getFixedAttractors <- function(net, genes, value, label, type="synchronous", returnDataFrame=TRUE) {
+    #     Simulates knocked-out or over-expression for all  genes
+    #     by fixing the values of genes to 0 or 1, 
+    #     Returns all the possible attractors with basin if posible
+    
+    # generate genes to evaluate
+    if (missing(genes) | missing(value)) { #Default, evaluate all single KOver
+        genes <- c(NA, net$genes, net$genes)
+        value <- c(NA, rep(0, length(net$genes)), rep(1, length(net$genes)))
+    }
+    if (missing(label)) {
+        label <- paste(genes, value, sep='_')
+        label[match('NA_NA', label)] <- 'WT'
+    }
+    
+    # Calculate mutants
+    mutants <- list()
+    for (i in 1:length(genes)) {
+        #print(paste(i, label[i], genes[i], value[i]  ))
+        if (!is.na(genes[i])) { net <- fixGenes(net, unlist(genes[i]), unlist(value[i])) }
+        attr <- getAttractors(net, type=type)
+        states <- getStatesAndBasin(attr)
+        mutants[[i]] <- states
+        if (! is.na(unlist(genes[i]))) net <- fixGenes(net, unlist(genes[i]), -1)
+    }
+    # convert to dataframe
+    names(mutants) <- label
+    if (returnDataFrame==TRUE) mutants <- fixedAttractorsListsToDataframe(mutants)
+    mutants
+}
+
+fixedAttractorsListsToDataframe <- function(attrList) {
+    # Receives a list of lists
+    # Each list corresponds to a network (with != fixed genes)
+    # Each list contains lists $states and $basin
+    # $states can cointain cyclic attractors
+    # Return a dataframe
+    
+    # Convert cycles to str
+    for (i in 1:length(attrList)) {
+        for (j in 1:length(attrList[[i]][[1]])) {
+            s <- attrList[[i]][[1]][[j]]
+            if (length(s) > 1) { s <- paste( s, collapse='-') }
+            attrList[[i]][[1]][[j]] <- s }
+        attrList[[i]][[1]] <- unlist(attrList[[i]][[1]])
+    }
+    
+    # Determine dataframe dimensions
+    states <- sapply(attrList, function(attr) attr$states)
+    states <- sort(unique(unlist(states)))
+    df <- data.frame(attr=states, row.names=states, stringsAsFactors = FALSE)
+    
+    #merge
+    for (name in names(attrList)) {
+        attr <- data.frame(attrList[[name]], row.names=attrList[[name]]$states, stringsAsFactors = FALSE)
+        names(attr) <- c('attr', name)
+        df <- merge(df, attr, all=TRUE)
+    }
+    df$attr <- as.character(df$attr)
+    row.names <- as.character(df$attr)
+    df
+}
